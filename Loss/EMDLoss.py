bf8c510c5de88583d082736975397b9be1f9f11b
badrminsar <86599580+badrminsar@users.noreply.github.com>
2023-08-18 01:02:08 +0200

    EMDLoss class

---
+++ Loss/EMDLoss.py
@@ -0,0 +1,18 @@
+import os, sys
+import tensorflow as tf
+from keras.losses import Loss
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, 'Ops\\approxmatch'))
+
+import tf_approxmatch
+
+class EMDLoss(Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, y_true, y_pred):
+        match = tf_approxmatch.approx_match(y_true, y_pred)
+        loss = tf.reduce_mean(tf_approxmatch.match_cost(y_true, y_pred, match))
+
+        return loss
