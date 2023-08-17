ce09035aca44e76f8c4fdebf19174c7fee54d623
badrminsar <86599580+badrminsar@users.noreply.github.com>
2023-08-18 01:15:57 +0200

    Extracting loss function as a compile parameter

--- Model/Autoencoder.py
+++ Model/Autoencoder.py
@@ -6,20 +6,17 @@ from tensorflow.keras.optimizers import Adam
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(BASE_DIR, 'Encoder'))
 sys.path.append(os.path.join(BASE_DIR, 'Decoder'))
-sys.path.append(os.path.join(BASE_DIR, '../Loss'))
 
 from PointNet import PointNet
 from FC import FC
-from CDLoss import ChamferDistanceLoss
 
 class PointNetAutoencoder:
     def __init__(self, input):
         self.input = input
         self.model = None
 
-    def compile(self, learning_rate=0.0001):
-        self.model.compile(optimizer=Adam(learning_rate=learning_rate),
-                           loss=ChamferDistanceLoss())
+    def compile(self, loss, learning_rate=0.0001):
+        self.model.compile(optimizer=Adam(learning_rate=learning_rate), loss=loss)
 
     def build(self):
         encoder_model = PointNet(self.input).build()
@@ -31,9 +28,3 @@ class PointNetAutoencoder:
         self.model = Model(inputs=self.input, outputs=decoded_tensor)
 
         return self.model
-
-if __name__ == "__main__":
-    input_tensor = Input(shape=(1024, 3, 1))
-    ae = PointNetAutoencoder(input_tensor)
-    ae.build()
-    ae.compile()
