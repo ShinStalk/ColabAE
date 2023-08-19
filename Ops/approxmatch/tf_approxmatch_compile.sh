d36d6e0022cb2770a062a3ecc194836e07a001d7
badrminsar <86599580+badrminsar@users.noreply.github.com>
2023-08-19 02:21:44 +0200

    Approxmatch sh commands update

--- Ops/approxmatch/tf_approxmatch_compile.sh
+++ Ops/approxmatch/tf_approxmatch_compile.sh
@@ -1,5 +1,4 @@
-#/bin/bash
+/usr/local/cuda/bin/nvcc /content/ColabAE/Ops/approxmatch/tf_approxmatch_g.cu -o /content/ColabAE/Ops/approxmatch/tf_approxmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-# TF1.4
-/usr/local/cuda-8.0/bin/nvcc tf_approxmatch_g.cu -o tf_approxmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+# TF>=1.4.0
+g++ -std=c++11 /content/ColabAE/Ops/approxmatch/tf_approxmatch.cpp /content/ColabAE/Ops/approxmatch/tf_approxmatch_g.cu.o -o /content/ColabAE/Ops/approxmatch/tf_approxmatch_so.so -shared -fPIC -I/usr/local/lib/python3.10/dist-packages/tensorflow/include/ -I/usr/local/lib/python3.10/dist-packages/tensorflow/include/external/nsync/public -L/usr/local/lib/python3.10/dist-packages/tensorflow -l:libtensorflow_framework.so.2 -I/usr/local/cuda/include -lcudart -L/usr/local/cuda/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=1
